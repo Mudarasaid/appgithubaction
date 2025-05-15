3276e8d969e629e592427bb3d4118b9c39d3a38e
Mudar Asaid <moudarasaid@gmail.com>
2025-05-15 13:40:18 +0200

    Fix test_sub to use sub() instead of add()

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,8 +5,8 @@ def test_add():
     assert add(-1,1)==0
     
 def test_sub():
-    assert add(5,3)==2
-    assert add(4,1)==1
-    assert add(3,3)==0
-    assert add(2,3)==-1
+    assert sub(5,3)==2
+    assert sub(4,1)==3
+    assert sub(3,3)==0
+    assert sub(2,3)==-1
     
